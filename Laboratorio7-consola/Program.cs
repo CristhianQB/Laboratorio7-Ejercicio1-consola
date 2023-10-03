f0c64fd60445c6d07d1993f7a6f39fcff1102a5f
CRISTHIAN <CRISTHIAN@DESKTOP-LS2ABPT>
2023-10-03 11:01:39 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio7-consola/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio7_consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el valor de n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            
+            float suma2 = 0;
+            for ( float i = 1 ; i <= n; i++)
+            {
+                float suma1 = (16 - i) / (3 * i); 
+                Console.WriteLine("resutado : " + suma1);
+                suma2 = suma2 + suma1;
+            }
+
+            Console.WriteLine(suma2);
+            Console.ReadKey();
+        }
+    }
+}
